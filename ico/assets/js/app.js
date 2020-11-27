341b38ee8393a22e7dbf83aeeb62549102469ef9
Đinh Quang Anh <56481830+dinhquanganh@users.noreply.github.com>
2020-11-27 17:45:41 +0700

    modal-mobile

--- ico/assets/js/app.js
+++ ico/assets/js/app.js
@@ -1,18 +1,39 @@
-function showModalState(stateIndex) {
-  $("#modal-state").modal({
-    show: "true",
-  });
-
-  $(".modal-content").each(function (index) {
-    $(this).hide();
-  });
-
-  $(`#content-state-${stateIndex}`).show();
+function showModalState(stateIndex,type) {
+  if (type==="web") {
+    $("#modal-state-web").modal({
+      show: "true",
+    });
+  
+    $(".modal-content").each(function (index) {
+      $(this).hide();
+    });
+  
+    $(`#content-state-${stateIndex}`).show();
+  } else if (type==="mobile") {
+    $("#modal-state-mobile").modal({
+      show: "true",
+    });
+  
+    $(".modal-content-mobile").each(function (index) {
+      $(this).hide();
+    });
+  
+    $(`#content-state-${stateIndex}-mobile`).show();
+  }
+  
 }
 
-var btnAction = (index) => {
-  $(".modal-content").each(function (index) {
-    $(this).hide();
-  });
-  $(`#content-state-${index}`).show();
+var btnAction = (index,type) => {
+  if (type==="mobile") {
+    $(".modal-content-mobile").each(function (index) {
+      $(this).hide();
+    });
+    $(`#content-state-${index}-mobile`).show();
+  } else {
+    $(".modal-content").each(function (index) {
+      $(this).hide();
+    });
+    $(`#content-state-${index}`).show();
+  }
+  
 };
