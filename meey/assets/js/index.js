73332a40f075d50ef6ec6eb4622fa4d32ee82dac
Đinh Quang Anh <56481830+dinhquanganh@users.noreply.github.com>
2020-12-04 06:15:29 +0700

    .add

--- meey/assets/js/index.js
+++ meey/assets/js/index.js
@@ -1,10 +1,20 @@
 // Function Produce
 let videoFunc = document.getElementById("videoFunction");
+let videoFuncMobile = document.getElementById("videoFunction-mobile");
 let myChildrenFunc = document.getElementsByClassName("child-content");
 let myArrowFunc = document.getElementsByClassName("arrow");
 let myIconFunc = document.getElementsByClassName("icon-function");
 var timeArray = [0, 5, 10, 15, 20, 25];
-videoFunc.ontimeupdate = actionChangeTimeline;
+if (window.matchMedia("(max-width: 872px)").matches) {
+  videoFunc.style.display = "none";
+  videoFuncMobile.style.display = "block";
+  videoFuncMobile.ontimeupdate = actionChangeTimelineMobile;
+} else {
+  videoFunc.style.display = "block";
+  videoFuncMobile.style.display = "none";
+  videoFunc.ontimeupdate = actionChangeTimeline;
+}
+
 function actionChangeTimeline() {
   let currentTime = videoFunc.currentTime;
   let chooseTime;
@@ -25,11 +35,33 @@ function actionChangeTimeline() {
     }
   }
 }
+function actionChangeTimelineMobile() {
+  let currentTime = videoFunc.currentTime;
+  let chooseTime;
+  for (let i = 0; i < timeArray.length; i++) {
+    if (i !== timeArray.length - 1) {
+      chooseTime =
+        currentTime >= timeArray[i] && currentTime < timeArray[i + 1];
+    } else {
+      chooseTime = currentTime >= timeArray[i];
+    }
+    if (chooseTime === true) {
+      removeActive();
+      for (let e = 0; e < myChildrenFunc.length; e++) {
+        myChildrenFunc[e].style.display = "none";
+      }
+      let thisDoc = document.querySelector(`div[data-time="${timeArray[i]}s"]`);
+      thisDoc.classList.add("active");
+      thisDoc.children[0].style.filter = "saturate(8)";
+      thisDoc.style.display = "block";
+      break;
+    }
+  }
+}
+
 function removeActive() {
   for (let e = 0; e < myChildrenFunc.length; e++) {
-    // if (myChildrenFunc[e].classList.indexOf("active")) {
     myChildrenFunc[e].classList.remove("active");
-    // }
   }
   for (let e = 0; e < myArrowFunc.length; e++) {
     myArrowFunc[e].style.display = "none";
@@ -40,10 +72,8 @@ function removeActive() {
 }
 (() => {
   for (let i = 0; i < myChildrenFunc.length; i++) {
-    console.log(myChildrenFunc[i]);
     myChildrenFunc[i].onclick = function () {
       let time = myChildrenFunc[i].getAttribute("data-time").replace("s", "");
-      console.log(time);
       videoFunc.currentTime = time;
     };
   }
@@ -59,7 +89,6 @@ function tgMenuLang() {
 window.addEventListener("scroll", () => {
   document.querySelector(".nav").classList.toggle("sticky", window.scrollY > 0);
 });
-
 
 const menuMobileToggle = document.querySelector(".menu-mobile");
 const menuMobileList = document.querySelector(".menu-mb");
